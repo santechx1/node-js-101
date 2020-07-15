b1c583d38b49b429cd4a383915ba486123759c08
santechx <santiagogf11@gmail.com>
2020-07-14 23:42:33 -0500

    Controllers and Libraries layers - MongoDB lib implementation - CRUD integration with mongo lib methods - Environment variables configured

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,9 +3,12 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
-moviesApi(app);
+// body parser
+app.use(express.json());
 
+moviesApi(app);
 app.listen(config.port, function () {
+  // eslint-disable-next-line no-console
   console.log(`Listening http://localhost:${config.port}`);
 });
 
