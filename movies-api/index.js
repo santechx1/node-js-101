80db1a20729c260ac06f85404e0807029008bda3
santechx <santiagogf11@gmail.com>
2020-07-16 00:23:43 -0500

    middleware for errors handling

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,11 +2,29 @@ const express = require('express');
 const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
+const {
+  logErrors,
+  wrapErrors,
+  errorHandler,
+} = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 // body parser
 app.use(express.json());
 
+//routes
 moviesApi(app);
+
+//catch 404
+app.use(notFoundHandler);
+
+
+// Errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
+
+
 app.listen(config.port, function () {
   // eslint-disable-next-line no-console
   console.log(`Listening http://localhost:${config.port}`);
